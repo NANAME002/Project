fefc9db0b8c8bf871f0c067b7bc2fe1a7f32f08a
NANAME002 <dennzerogo@gmail.com>
2024-11-16 12:02:15 +0200

    add 1

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+        public static void Func()
+        {
+            Console.Write("Ввод a= ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Ввод b= ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{a}+{b}={a + b}");
+        }
+    }
+}
