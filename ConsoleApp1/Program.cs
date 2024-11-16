238831f9205de14616052ef52945a2035e3c0cac
NANAME002 <dennzerogo@gmail.com>
2024-11-16 11:51:47 +0200

    add 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello();
+            SayBay();
+            Console.ReadLine();
+            
+
+        }
+        public static void SayHello()
+        {
+            Console.WriteLine("Привет");
+        }
+        public static void SayBay()
+        {
+            Console.WriteLine("Пока");
+        }
+    }
+}
